849c41110f92540e1567f62c3c5fc88bf26d8f77
Beth Carretta <bethcarretta@gmail.com>
2023-09-01 14:16:22 -0400

    hasDuplicates, vowelCount complete

--- maps-sets.js
+++ maps-sets.js
@@ -26,8 +26,29 @@ m.set([1,2,3], false);
 hasDuplicate([1, 3, 2, 1]) // true
 hasDuplicate([1, 5, -1, 4]) // false
 
+function hasDuplicate(arr) {
+  return new Set(arr).size !== arr.length
+}
+
 // vowelCount
 // Write a function called vowelCount which accepts a string and returns a map where the keys are numbers and the values are the count of the vowels in the string.
 
-vowelCount('awesome') // Map { 'a' => 1, 'e' => 2, 'o' => 1 }
-vowelCount('Colt') // Map { 'o' => 1 } */
+console.log(vowelCount('awesome')) // Map { 'a' => 1, 'e' => 2, 'o' => 1 }
+console.log(vowelCount('Colt')) // { 'o' => 1 }
+
+function vowelCount(str) {
+  const countMap = new Map()
+  const vowels = 'aeiou'
+
+  for (let char of str.toLowerCase()) {
+    if (vowels.includes(char)) {
+      if (countMap.has(char)) {
+        countMap.set(char, countMap.get(char) + 1)
+      } else {
+        countMap.set(char, 1)
+      }
+    }
+  }
+
+  return countMap
+}
